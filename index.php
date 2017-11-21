da5e89b4a3b8aaf00e83020fd2d901498f3f4762
Mrgove10 <25491408+Mrgove10@users.noreply.github.com>
2017-11-21 16:51:33 +0100

    apres le caca de lou

--- index.php
+++ index.php
@@ -14,12 +14,39 @@
 
     </header>
     <section class="MainTime">
-        <?php
-        echo strftime('%A %d %B %Y');?>
-        <br>
-        <?php
-        echo strftime('%H:%M');
-        ?>
+        <script>
+        function date_heure(id)
+        {
+            date = new Date;
+            annee = date.getFullYear();
+            moi = date.getMonth();
+            mois = new Array('Janvier', 'F&eacute;vrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Ao&ucirc;t', 'Septembre', 'Octobre', 'Novembre', 'D&eacute;cembre');
+            j = date.getDate();
+            jour = date.getDay();
+            jours = new Array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
+            h = date.getHours();
+            if(h<10)
+            {
+                h = "0"+h;
+            }
+            m = date.getMinutes();
+            if(m<10)
+            {
+                m = "0"+m;
+            }
+            s = date.getSeconds();
+            if(s<10)
+            {
+                s = "0"+s;
+            }
+            resultat = jours[jour]+' '+j+' '+mois[moi]+' '+annee+'<br>'+h+':'+m+':'+s;
+            document.getElementById(id).innerHTML = resultat;
+            setTimeout('date_heure("'+id+'");','1000');
+            return true; 
+        }
+        </script>
+        <span id="date_heure"></span>
+        <script type="text/javascript">window.onload = date_heure('date_heure');</script>
     </section>
     <section class="Sites">
         <ul class="divers">
